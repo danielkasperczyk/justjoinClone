2367840cc2000488810b1e236779f6d318e45016
danielkasperczyk <75161634+danielkasperczyk@users.noreply.github.com>
2021-02-26 12:49:33 +0100

    Added Header.vue and Nav.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-layout align-center justify-space-between>
+    <h2>justget.it</h2>
+    <v-switch
+      label="Dark mode"
+      v-model="$vuetify.theme.dark"
+      hide-details
+    ></v-switch>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style></style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-container fluid>
+    <v-form class="d-flex align-center">
+      <v-btn fab small>
+        <v-icon>fas fa-search</v-icon>
+      </v-btn>
+      <v-select
+        v-model="selectedLocation"
+        class="flex-sm-grow-0 mx-5"
+        :items="locations"
+      ></v-select>
+      <v-select
+        v-model="selectedTech"
+        class="flex-sm-grow-0"
+        :items="tech"
+      ></v-select>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  data() {
+    return {
+      selectedLocation: '',
+      selectedTech: '',
+      locations: [
+        'Warszawa',
+        'Kraków',
+        'Wrocław',
+        'Poznań',
+        'Trójmiasto',
+        'Śląsk',
+      ],
+      tech: [
+        'HTML',
+        'JavaScript',
+        'PHP',
+        'Ruby',
+        'Python',
+        'Java',
+        '.Net',
+        'C',
+        'UX/UI',
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
